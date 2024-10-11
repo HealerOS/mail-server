b6cff57d2fb2b2f44492ef16d108103dbaf48ed9
Jason <1145969453@qq.com>
2024-10-11 23:45:47 +0800

    支持解析post传入的参数

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,17 @@
+use actix_web::web::Form;
 use actix_web::HttpResponse;
+use serde::Deserialize;
 
 pub async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+#[derive(Debug, Deserialize)]
+pub struct UserInfo {
+    username: String,
+    email: String,
+}
 
-pub async fn subscribe() -> HttpResponse {
+pub async fn subscribe(user_info: Form<UserInfo>) -> HttpResponse {
+    println!("{:?}", user_info);
     HttpResponse::Ok().finish()
 }
